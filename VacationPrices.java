f466775a7ba49157a4d8fb07c25b2ce8b47c0cc5
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-02-27 21:26:06 -0500

    Commit

--- VacationPrices.java
+++ VacationPrices.java
@@ -1,7 +1,7 @@
 /**
  *
  */
-package day6;
+//package day6;
 import java.util.Scanner;
 
 /**
@@ -18,14 +18,25 @@ public class VacationPrices {
 	 */
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("====================================================");
+		System.out.println("Please enter the following information".toUpperCase());
+		System.out.print("Duration = ");
 		int duration = scanner.nextInt();
+		System.out.print("Food = $");
 		int food = scanner.nextInt();
+		System.out.print("Flight = $");
 		int flight = scanner.nextInt();
+		System.out.print("Night = $");
 		int night = scanner.nextInt();
+		System.out.println("====================================================");
 		scanner.close();
 
 		int result = (food * duration) + (flight * 2) + night * (duration - 1);
 
-		System.out.println(result);
+		System.out.println("====================================================");
+		System.out.println("Overall your vacation time will cost you: $" + result);
+		System.out.println("====================================================");
+
+		System.out.println("HAPPY VACATION!\nENJOY!!!");
 	}
 }
